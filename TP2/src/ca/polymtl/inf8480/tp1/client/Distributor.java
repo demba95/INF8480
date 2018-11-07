17bd10b9eddac653843894e11692f6b333bc5a07
Papa Demba Tall <papa-demba.tall@polymtl.ca>
2018-11-07 02:28:07 -0500

    Ajout fichier config et implementation de safeRun()

--- TP2/src/ca/polymtl/inf8480/tp1/client/Distributor.java
+++ TP2/src/ca/polymtl/inf8480/tp1/client/Distributor.java
@@ -27,11 +27,74 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 import ca.polymtl.inf8480.tp1.shared.ServerInterface;
+import ca.polymtl.inf8480.tp1.shared.Configurations;
 import ca.polymtl.inf8480.tp1.shared.FileContent;
+
+//Refs: http://tutorials.jenkov.com/java-concurrency/creating-and-starting-threads.html
+//http://www.java2novice.com/java_thread_examples/implementing_runnable/
+//plus simple methode pour gerer les threads
+public class RunnableThread implements Runnable {
+    public ArrayList<FileContent> listCalculs_;
+    public int calculResults = 0;
+    private int nOpsEach_ = 5;
+    public int count_ = 0;
+    private ServerInterface stubServer_;
+
+    public RunnableThread(ArrayList<FileContent> liste, int ops, ServerInterface stubs) {
+        this.listCalculs_ = liste;
+        this.stubServer_ = stubs;
+        this.count_ = ops;
+    }
+
+    public void run() {
+
+        int nbOperations = this.listCalculs_.size(); // get number of calculs to send
+
+        while (nbOperations != 0) {
+            ArrayList<FileContent> data = new ArrayList<FileContent>(); // data sent to server
+            for (int i = 0; i < this.nOpsEach_; i++) {
+                if (i < nbOperations()) {
+                    data.add(this.Taches.get(i));
+                }
+            }
+            // get results
+            ArrayList<Integer> theResults = null;
+            try {
+                theResults = this.stubServer_.processOperations(data); // calculer les operations
+            } catch (Exception e) {
+                this.calculResults = -1;
+                System.out.println("Error: " + e.getMessage());
+                return;
+            }
+
+            checkResults(data, theResults);
+        }
+    }
+
+    private void checkResults(ArrayList<FileContent> data, ArrayList<Integer> myResults) {
+        if (myResults == null) {
+            // cannot reduce
+            this.nOpsEach_--;
+        } else if (myResults != null) {
+            // On est capable de traiter encore plus de taches par block (peut-etre)
+            this.nOpsEach_++;
+
+            for (Integer each : myResults) {
+                this.calculResults = (this.calculResults + each) % 4000;
+            }
+            this.listCalculs_.removeAll(data); // remove selected data from what to send
+        }
+    }
+}
 
 public class Distributor {
     ArrayList<FileContent> listOfOperations;
-    ArrayList<Integer> operationResults;
+
+    // private ServerInterface[] listOfServersStubs = new ServerInterface[4]; // 4
+    // servers stubs because 4 is the maxNumber
+    // of servers used in this work
+    private ArrayList<Configurations> listOfServersStubs;// stub Servers;
+    ArrayList<Configurations> connectedServers;// connected Servers
 
     public static void main(String[] args) {
         String security = "";
@@ -65,6 +128,24 @@ public class Distributor {
         }
     }
 
+    public void readServersConfigurations(String fileName) {
+        try {
+            connectedServers.clear();
+            BufferedReader br = new BufferedReader(fileName);
+            for (String task; (task = br.readLine()) != null;) {
+                String[] chaine = task.split("\t"); // divise ligne en 4 params
+                String serverAddress = chaine[0];
+                int portNumber = (int) Integer.parseInt(chaine[1]);
+                String mode = chaine[2];
+                int capacity = (int) Integer.parseInt(chaine[3]);
+                connectedServers.add(new Configurations(serverAddress, mode, portNumber, capacity));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
     public void readOperationsFile(String fileName) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -73,6 +154,7 @@ public class Distributor {
                 FileContent theTask = new FileContent(chaine[0], (int) Integer.parseInt(chaine[1]));
                 listOfOperations.add(theTask);
             }
+            br.close();
         } catch (FileNotFoundException e) {
             System.err.println("Error opening file: " + e.getMessage());
         } catch (Exception e) {
@@ -82,7 +164,84 @@ public class Distributor {
     }
 
     public void safeRun() {
+        Boolean remaining = true;
+        ArrayList<RunnableThread> runnableThreads = new ArrayList<RunnableThread>();
+        ArrayList<Thread> simpleThreads = new ArrayList<Thread>();
+        int nbConnectedServers = listOfServersStubs.size();
 
+        int nbTacheUnit = listOfOperations.size() / nbConnectedServers; // nb de taches par serveur
+        int results = 0;
+
+        while (remaining) {
+            remaining = false;
+            int i = 0;
+            int j = 1;
+            runnableThreads.clear();
+            simpleThreads.clear();
+            // Use a thread for each server
+            for (ServerInterface stubServer : listOfServersStubs) {
+                ArrayList<Pair> opTasks = new ArrayList<Pair>();
+                int offset = 0;
+                if (j == nbConnectedServers && nbConnectedServers != 1) {
+                    offset = listOfOperations.size() % nbConnectedServers;
+                }
+                for (; i < (nbTacheUnit * j) + offset; i++) {
+                    opTasks.add(listOfOperations.get(i));
+                }
+                threadServeur threadClass = new threadServeur(opTasks, stub, j - 1);
+                runnableThreads.add(threadClass);
+                Thread thread = new Thread(threadClass);
+                simpleThreads.add(thread);
+                thread.start();
+                j++;
+            }
+            listOfOperations.clear();
+
+            try {
+                results = commuteCalculResults(simpleThreads, runnableThreads);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Final result = " + results);
+
+        // int k = 0;
+        // for (Thread thread : simpleThreads) {
+        // thread.join();
+        // // Si un serveur a crash on repartit ses taches avec les autres serveurs
+        // if (runnableThreads.get(k).getResult() == -1) {
+        // remaining = true;
+        // listOfOperations.addAll(runnableThreads.get(k).listCalculs_);
+        // listOfServersStubs.remove(runnableThreads.get(k).count_);
+        // }
+        // // Si le serveur a reussi
+        // else {
+        // results = (results + runnableThreads.get(k).calculResults) % 4000;
+        // }
+        // k++;
+        // }
+
+    }
+
+    public int commuteCalculResults(ArrayList<Thread> threadList, ArrayList<RunnableThread> myRunnable) {
+        int k = 0;
+        int calculResult = 0;
+        for (Thread thread : threadList) {
+            thread.join();
+            // Si un serveur a crash on repartit ses taches avec les autres serveurs
+            if (myRunnable.get(k).getResult() == -1) {
+                remaining = true;
+                this.listOfOperations.addAll(myRunnable.get(k).listCalculs_);
+                this.listOfServersStubs.remove(myRunnable.get(k).count_);
+            }
+            // Si le serveur a reussi
+            else {
+                calculResult = (calculResult + myRunnable.get(k).calculResults) % 4000;
+            }
+            k++;
+        }
+        return calculResult;
     }
 
     public void nonSafeRun() {
